0bea6869b4df132ec2b0af9cd9b3c50b838a8fce
Josh Ransom <josh.bh.ransom@gmail.com>
2015-08-28 16:24:47 -0700

    commit to show michael

--- client/app/Maint/Maint.controller.spec.js
+++ client/app/Maint/Maint.controller.spec.js
@@ -2,20 +2,7 @@
 
 describe('Controller: MaintCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('fullGridApp'));
-
-  var MaintCtrl, scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MaintCtrl = $controller('MaintCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should ...', function () {
+  it('hard in the Maint', function () {
     expect(1).toEqual(1);
   });
 });
